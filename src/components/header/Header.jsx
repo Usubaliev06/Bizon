5b76d3e7a698d26ce06180b2df45861515f1c309
adil <adilusubaliev@mail.ru>
2023-04-02 20:27:44 +0600

    fix bug9

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import css from "./Header.module.css";
-
+import React from 'react';
 import { NavLink } from "react-router-dom";
 import logo from "../../images/logo.JPG";
 import { useState } from "react";
